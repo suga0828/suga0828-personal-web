d786fc27f47570aeb75eb12cd751560afe5b0667
suga0828 <alexander.sf0828@gmail.com>
2018-10-26 20:51:53 -0500

    HeaderComponent and FooterComponent created. Added styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 
+import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -14,7 +17,10 @@ import {MatButtonModule, MatCheckboxModule} from '@angular/material';
     BrowserModule,
 
     BrowserAnimationsModule,
-    MatButtonModule, MatCheckboxModule
+    MatButtonModule, MatCheckboxModule,
+
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
