979145c7a77faded8f3f128605c408b159eae5b3
suga0828 <alexander.sf0828@gmail.com>
2018-11-15 14:01:54 -0500

    Styles added to the footer with icons. Var colors move to main.css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,14 +9,6 @@ import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faTwitter, faGithub, faMedium, faMediumM } from '@fortawesome/free-brands-svg-icons';
-
-// Add an icon to the library for convenient access in other components
-library.add(faTwitter, faGithub, faMedium, faMediumM);
-
-
 @NgModule({
   declarations: [
     AppComponent
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,8 +4,16 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTwitterSquare, faGithubSquare, faLinkedin, faMedium } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelopeSquare } from '@fortawesome/free-solid-svg-icons';
+
+// Add an icon to the library for convenient access in other components
+library.add(faTwitterSquare, faGithubSquare, faLinkedin, faMedium, faEnvelopeSquare);
+
 @NgModule({
-	imports: [CommonModule],
+	imports: [CommonModule, FontAwesomeModule],
 	declarations: [HeaderComponent, FooterComponent],
 	exports:  [HeaderComponent, FooterComponent]
 })
