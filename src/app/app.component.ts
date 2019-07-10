ab40dae33b37691b05fecc4ec814ff265557eaf5
Alexander Sandoval <alexander.sf0828@gmail.com>
2019-07-09 19:32:06 -0500

    Refactor navigation. (WIP)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,5 @@
 import { Component } from '@angular/core';
 
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,10 +8,5 @@ import { map } from 'rxjs/operators';
 export class AppComponent {
   title = 'suga0828';
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches)
-    );
-
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor() {}
 }
