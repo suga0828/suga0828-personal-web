08f73bccb177eae2e73c5a114fb699a0185833e0
Alexander Sandoval <alexander.sf0828@gmail.com>
2019-07-19 14:06:58 -0500

    Move forward in header styles. (WIP)

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,14 +7,14 @@ import { FooterComponent } from './footer/footer.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faTwitterSquare, faGithubSquare, faLinkedin, faMedium } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelopeSquare } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelopeSquare, faBars } from '@fortawesome/free-solid-svg-icons';
 
 import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
 import { SidenavListComponent } from './sidenav-list/sidenav-list.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 // Add an icon to the library for convenient access in other components
-library.add(faTwitterSquare, faGithubSquare, faLinkedin, faMedium, faEnvelopeSquare);
+library.add(faTwitterSquare, faGithubSquare, faLinkedin, faMedium, faEnvelopeSquare, faBars);
 
 @NgModule({
 	imports: [CommonModule, FontAwesomeModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, FlexLayoutModule ],
