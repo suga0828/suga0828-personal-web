a59605c25de36747fc6c6734f0e00bb141bee40a
Alexander Sandoval <alexander.sf0828@gmail.com>
2019-07-20 19:17:26 -0500

    Delete unnecesary styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
-
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
@@ -17,10 +17,12 @@ import { SharedModule } from './shared/shared.module';
     BrowserAnimationsModule,
 
     AppRoutingModule,
-    
+
     SharedModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
