5e762f19bc1587db81bc4a62be8988da5d71f16e
Alexander Sandoval <alexander.sf0828@gmail.com>
2020-10-21 23:09:20 -0500

    close sidenav on desktop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,27 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, HostListener, ViewChild, ViewEncapsulation } from '@angular/core';
+
+import { MediaMatcher } from '@angular/cdk/layout';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
-export class AppComponent { }
+export class AppComponent {
+  desktopQuery: MediaQueryList;
+
+  @ViewChild('sidenav') sidenav: MatSidenav;
+
+  constructor(media: MediaMatcher) {
+    this.desktopQuery = media.matchMedia('(min-width: 600px)');
+  }
+
+  @HostListener('window:resize', [])
+  private onResize(): void {
+    if (this.sidenav.opened && this.desktopQuery.matches) {
+      this.sidenav.close();
+    }
+  }
+}
